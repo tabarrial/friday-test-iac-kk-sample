38d8a12b6ae8888e78acbef6c51e65188475bf5b
Alberto <alberto@alberto.chat>
2021-07-08 13:34:57 +0200

    ajustes

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "ec2-test" {
   name                   = "ec2-test"
   instance_count         = 1
   ami                    = "ami-0a8e758f5e873d1c1"
-  instance_type          = "t3.micro"
+  instance_type          = "t3a.medium"
   key_name               = "friday-key"
   vpc_security_group_ids = ["sg-64402500"]
   subnet_id              = "subnet-f20044ab"
@@ -15,3 +15,9 @@ module "ec2-test" {
 }
 
 }
+
+output "ec2-test_public_ip" {
+  description = "Public IP instance"
+  value       = module.ec2-test.public_ip
+}
+
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ec2-test_public_ip" {
-  description = "Public IP instance"
-  value       = module.ec2-test.public_ip
-}
