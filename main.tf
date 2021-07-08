ea0d990916845eb92783e33412c003a5d93e7747
tabarrial <79221432+tabarrial@users.noreply.github.com>
2021-07-08 14:39:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "ec2-test" {
   name                   = "ec2-test"
   instance_count         = 1
   ami                    = "ami-0a8e758f5e873d1c1"
-  instance_type          = "t3a.medium"
+  instance_type          = "t3a.micro"
   key_name               = "friday-key"
   vpc_security_group_ids = ["sg-64402500"]
   subnet_id              = "subnet-f20044ab"
